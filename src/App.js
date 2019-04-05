7ce13e01063d378a89dcce306044c5d37a1d318d
tomgiagtz <tom.giagtzoglou@gmail.com>
2019-04-04 21:50:54 -0400

    basic containers outline

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        
+        <Route path= '/home' Component={HomeContainer}></Route>
       </div>
+      </Router>
     );
   }
 }
