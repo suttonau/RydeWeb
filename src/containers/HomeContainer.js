cacb1b7962935619fcd6e60e6fcc8bc139b40840
Sean Sutton Jr <sutton_mjbc@icloud.com>
2019-04-06 23:00:33 -0400

    adding tabs to search bar #time 1.5h

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import SearchComponent from '../components/SearchComponent';
+import SearchComponent from '../components/SearchComponent'
 
 export default function HomeContainer() {
   return (
-	<div>
-	  <SearchComponent />
-	</div>
+    <div>
+      <div> HEADER IMG </div>
+      <SearchComponent />
+    </div>
   )
 }
