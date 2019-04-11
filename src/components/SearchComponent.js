f6b9648d958fd053efd619194e3854257e7f4d13
tomgiagtz <tom.giagtzoglou@gmail.com>
2019-04-11 00:17:30 -0400

    correct layout of searchbar

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Grid, Input } from 'semantic-ui-react'
 import './styles/Search.css'
 
 const CURRENT_YEAR = new Date().getFullYear()
@@ -31,27 +31,48 @@ export default class SearchComponent extends Component {
     return (
       <div>
         <div className="search-container">
-          <Button.Group fluid widths="3" color="blue">
-            <Button attached="top" onClick={() => this._renderTabContent(DEFAULT)}>
-              Tab1
-            </Button>
-            <Button attached="top" onClick={() => this._renderTabContent(TAB2)}>
-              Tab 2
-            </Button>
-            <Button attached="top" onClick={() => this._renderTabContent(TAB3)}>
-              Tab 3
-            </Button>
-          </Button.Group>
-          <Form className="form">
-            <Form.Group>
-              <Form.Input label="Make" placeholder="Make" />
-              <Form.Input label="Model" placeholder="Model" />
-              <Form.Input label="Year" type="number" placeholder={CURRENT_YEAR} />
-            </Form.Group>
-            <Form.Group>
-              <Form.Input label="Zip Code" placeholder="Zip Code" />
-            </Form.Group>
-            <Form.Button>Submit</Form.Button>
+          <Form>
+          <Grid stackable columns='equal'>
+            <Grid.Row>
+              {/* <Button.Group fluid widths="3" color="blue"> */}
+              <Grid.Column>
+                <Button attached="top" onClick={() => this._renderTabContent(DEFAULT)}>
+                  Tab1
+                </Button>
+              </Grid.Column>
+              <Grid.Column>
+                <Button attached="top" onClick={() => this._renderTabContent(TAB2)}>
+                  Tab 2
+                </Button>
+              </Grid.Column>
+              <Grid.Column>
+                <Button attached="top" onClick={() => this._renderTabContent(TAB3)}>
+                  Tab 3
+                </Button>
+              </Grid.Column>
+              {/* </Button.Group> */}
+            </Grid.Row>
+
+            <Grid.Row>
+              <Grid.Column>
+                <Input label="Make" placeholder="Make" fluid />
+              </Grid.Column>
+              <Grid.Column>
+                <Input label="Model" placeholder="Model" fluid />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <Input label="Year" type="number" placeholder={CURRENT_YEAR} fluid />
+              </Grid.Column>
+              <Grid.Column>
+                <Input label="Zip Code" placeholder="Zip Code" fluid />
+              </Grid.Column>
+              <Grid.Column width={4}>
+                <Form.Button fluid  >Search</Form.Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
           </Form>
         </div>
       </div>
